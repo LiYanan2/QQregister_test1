07ac408219c43cdfb5a30db711d74c53f40ba2b5
superNN <liiyn@foxmail.com>
2018-03-28 20:05:31 +0800

    修改.xml实现了页面布局和数据的分离，修改MainActivity.java状态栏的沉浸式实现

--- app/src/main/java/xyz/liyanan/qqregister_test1/MainActivity.java
+++ app/src/main/java/xyz/liyanan/qqregister_test1/MainActivity.java
@@ -1,7 +1,11 @@
 package xyz.liyanan.qqregister_test1;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
 }
